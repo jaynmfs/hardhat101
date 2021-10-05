ec5bd860be9b8407b7607641979de9532ce4890d
jaynmfs <jane_marfaresys@outlook.com>
2021-10-05 14:54:35 +0700

    Support connect to external network

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,10 +9,20 @@ task('accounts', 'Display accounts', async (args, hre) => {
   }
 });
 
+const mnemonic = ``; // opt 1
+// const privateKey = ``; // opt 2
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   // solidity: "0.7.3",
   solidity: "0.8.7",
+  networks: {
+    ropsten: {
+      url: '', // get form alchemy.com
+      accounts: { mnemonic }, // opt 1
+      // accounts: [privateKey] // opt 2
+    }
+  }
 };
